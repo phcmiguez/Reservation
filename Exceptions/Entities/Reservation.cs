df5723c0697602cb5d9b83e128d3c6398d5c5650
Pedro Henrique Costa Miguez <pedrohcmiguez@gmail.com>
2023-02-25 21:33:17 -0300

    Add project files.

---
+++ Exceptions/Entities/Reservation.cs
@@ -0,0 +1,59 @@
+﻿using Exceptions.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation() { }
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkIn > checkOut)
+            {
+                throw new DomainException("The check-out date must be after check-in date.");
+            }
+
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            return CheckOut.Subtract(CheckIn).Days;
+        }
+
+        public void UpdateDates(DateTime checkin, DateTime checkout)
+        {
+            try
+            {
+                if(checkin > checkout)
+                {
+                    throw new DomainException("The check-out date must be after check-in date.");
+                }
+
+                CheckIn = checkin;
+                CheckOut = checkout;
+
+                Console.WriteLine("Dates updated successfully.");
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine("Erro! " + e.Message);
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Room " + RoomNumber + ", " + "check-in: " + CheckIn.ToString("dd/MM/yyyy") + ", " + "check-out: " + CheckOut.ToString("dd/MM/yyyy") + ", " + Duration() + " nights.";
+        }
+    }
+}
---
+++ Exceptions/Program.cs
@@ -0,0 +1,46 @@
+﻿using Exceptions.Entities;
+using Exceptions.Entities.Exceptions;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+                Console.WriteLine("Room Reservation Program");
+                Console.Write("Insert the room number: ");
+                int room = int.Parse(Console.ReadLine());
+                Console.Write("Checkin (DD/MM/YYYY): ");
+                DateTime checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Checkout (DD/MM/YYYY): ");
+                DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new(room, checkin, checkout);
+
+                Console.WriteLine("Would you like to update? (y/n)");
+                char choice = char.Parse(Console.ReadLine());
+
+                if (choice == 'y')
+                {
+                    Console.Write("Please insert the new checkin date (DD/MM/YYYY): ");
+                    checkin = DateTime.Parse(Console.ReadLine());
+                    Console.Write("Please insert the new checkout date (DD/MM/YYYY): ");
+                    checkout = DateTime.Parse(Console.ReadLine());
+
+                    reservation.UpdateDates(checkin, checkout);
+                }
+
+                Console.WriteLine("\nReservation Details");
+                Console.WriteLine("-------------------");
+                Console.WriteLine(reservation.ToString());
+
+            }
+			catch (DomainException e)
+			{
+                Console.WriteLine("Error in reservation: " + e.Message); ;
+			}
+        }
+    }
+}
